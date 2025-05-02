1f47583ef748cdc69035f38806489e4a12fa9e7b
Adarsh Hegde <ch22b048@smail.iitm.ac.in>
2025-05-02 06:29:39 +0530

    Server Files

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -22,7 +22,10 @@ AUTH_USER_MODEL = 'useraccount.User'
 
 SITE_ID = 1
 
-WEBSITE_URL = 'http://localhost:8000'
+if DEBUG:
+    WEBSITE_URL = 'http://localhost:8000'
+else:
+    WEBSITE_URL = 'http://157.245.99.250:1337'
 
 CHANNEL_LAYERS = {
     'default' : {
@@ -60,6 +63,24 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
+    'http://157.245.99.250',
+    'http://157.245.99.250:1337'
+]
+
+CORS_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    'http://157.245.99.250',
+    'http://157.245.99.250:1337'
+]
+
+CORS_ORIGINS_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    'http://157.245.99.250',
+    'http://157.245.99.250:1337'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
